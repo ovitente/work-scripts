0698fcd85738e715a9a8685e313ed7583f6e00fe
Ivan Kostrubin <7spawn@gmail.com>
2021-10-15 14:16:21 +0300

    a little speedup

--- ec2-snapshots/ec2-snapshots.sh
+++ ec2-snapshots/ec2-snapshots.sh
@@ -9,11 +9,11 @@ JSON_SOURCE="snapshots.json"
 ARRAY_INDEX=""
 
 # Cleaning before new start
-rm -rf $JSON_SOURCE
+# rm -rf $JSON_SOURCE
 # Get json with spanshots
-aws ec2 describe-snapshots \
-  --owner-ids ${CREATOR_ID} \
-  --query "Snapshots[*].{ID:SnapshotId,CreationTime:StartTime,Source:VolumeId,Size:VolumeSize}" > ${JSON_SOURCE}
+# aws ec2 describe-snapshots \
+#   --owner-ids ${CREATOR_ID} \
+#   --query "Snapshots[*].{ID:SnapshotId,CreationTime:StartTime,Source:VolumeId,Size:VolumeSize}" > ${JSON_SOURCE}
 
 # Count elements in the json starting with [0]
 ARRAY_INDEX=$(grep -o -i ID ${JSON_SOURCE} | wc -l)
@@ -25,12 +25,14 @@ echo "-----------------------|---------------------|-----------------------|----
 i=0
 while [ $i -ne $ARRAY_INDEX ]
 do
-  id=$(cat ${JSON_SOURCE}            | jq .[$i].ID -r)
-  creation_time=$(cat ${JSON_SOURCE} | jq .[$i].CreationTime -r | cut -b 1-19)
-  snap_source=$(cat ${JSON_SOURCE}   | jq .[$i].Source -r)
-  size=$(cat ${JSON_SOURCE}          | jq .[$i].Size -r)
+  # id=$(cat ${JSON_SOURCE}            | jq .[$i].ID -r)
+  # creation_time=$(cat ${JSON_SOURCE} | jq .[$i].CreationTime -r | cut -b 1-19)
+  # snap_source=$(cat ${JSON_SOURCE}   | jq .[$i].Source -r)
+  # size=$(cat ${JSON_SOURCE}          | jq .[$i].Size -r)
 
-  echo "$id | $creation_time | $snap_source | $size"
+  jq -r '[.['"$i"'].ID, .['"$i"'].CreationTime, .['"$i"'].Source, .['"$i"'].Size] | @tsv' snapshots.json
+
+  # echo "$id | $creation_time | $snap_source | $size"
 
   i=$(($i+1))
 done
